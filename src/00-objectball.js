00d4e372e57d52574cffe6ed9aaecdc771ee5252
connorq <connorquitt@gmail.com>
2023-07-22 14:08:27 -0500

    hi

---
+++ src/00-objectball.js
@@ -0,0 +1,19 @@
+function gameObject() {
+    let obj = {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["black", "white"],
+            players: {
+                "alan Anderson": {
+                    number: "0",
+                    shoe: "16",
+                    points: "22",
+                    rebounds: "12",
+                    assists: "12",
+                    
+                }
+            }
+        }
+        away:
+    }
+}
